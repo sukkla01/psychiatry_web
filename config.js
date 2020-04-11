cb058b56e3e9abdcccd1b38de6256c6aa20a74bd
sukkla01 <sukkla01@gmail.com>
2020-04-11 20:58:44 +0700

    2

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
     font: 'Kanit',
     test:'',
-    // BASE_URL:'http://110.49.126.23:4001',
+    BASE_URL:'http://swdata.srisangworn.go.th:4033',
     // BASE_URL:'http://192.168.0.59:4044',
-    BASE_URL:'http://localhost:4000',
+    // BASE_URL:'http://localhost:4000',
     height_monitor : 800,
     width_heating : '95%',
     width_observ : '95%',
